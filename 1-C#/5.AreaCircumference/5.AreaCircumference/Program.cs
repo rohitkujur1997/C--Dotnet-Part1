30dfed150e4f7823c5e3eefe1bf60c5cc3846ff6
unknown <kujur.rohit95@gmail.com>
2022-04-07 05:19:33 +0530

    commiting 1st assignment 5th problem

---
+++ 1-C#/5.AreaCircumference/5.AreaCircumference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.AreaCircumference
+{
+    internal class Program
+    {
+        static void Solution(float r)
+        {
+            float area, circumference, pie=3.14F;
+            area = pie * r * r;
+            Console.WriteLine("Area of Circle {0}", area);
+            circumference = 2 * pie * r;
+            Console.WriteLine("Circumference of Circle {0}", circumference);
+        }
+        static void Main(string[] args)
+        {
+            float r;
+            Console.WriteLine("Enter Circle Radius: ");
+            r= Single.Parse(Console.ReadLine());
+            Solution(r);
+        }
+    }
+}
