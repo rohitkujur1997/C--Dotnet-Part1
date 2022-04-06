5be6e2409f595a53c6d3d1295689dd6fcb52445d
unknown <kujur.rohit95@gmail.com>
2022-04-07 04:41:49 +0530

    commiting 1st assignment 3rd problem

---
+++ 1-C#/3.CallMethod/3.CallMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.CallMethod
+{
+    internal class Program
+    {
+        static void ClientMethod(int[] integer)
+        {
+            int sum=0;
+            for (int i = 0; i < integer.Length; i++)
+                sum += integer[i];
+            Console.WriteLine("Sum of all integers {0}", sum);
+        }
+        static void Main(string[] args)
+        {
+            int[] integer = new int[5];
+            for (int i = 0; i < integer.Length; i++)
+                integer[i] = Convert.ToInt32(Console.ReadLine());
+            ClientMethod(integer);
+        }
+    }
+}
